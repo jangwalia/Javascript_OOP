4d0e70db3b7d9d34d6cb91cb0cd21612be33654d
jang walia <jangiwalia83@gmail.com>
2022-04-05 16:48:48 -0700

    add Student class

--- classes_in_javascript.js
+++ classes_in_javascript.js
@@ -1,2 +1,12 @@
 //In javascript we class keyword to create new class
+class Student {
+  //constructor defines what will be the values of each objects created 
+  constructor(firstname,lastname,year) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.year = year
+  }
 
+}
+let student1 = new Student("Jang","walia",2022);
+console.log(student1.firstname);
